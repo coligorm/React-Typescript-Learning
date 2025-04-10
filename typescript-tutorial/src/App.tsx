53b39d7bd9a729a6bbbf46585d8a32fe1e3e7d9c
coligorm <colinrhysgorman@gmail.com>
2025-04-10 18:30:04 +1000

    - Created todoList - Added handleAdd for adding a todo to the todoList

--- typescript-tutorial/src/App.tsx
+++ typescript-tutorial/src/App.tsx
@@ -7,12 +7,21 @@ const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
   const [todoList, setTodoList] = useState<Todo[]>([]);
 
-  console.log(todo);
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodoList([...todoList, { id: Date.now(), todo, isDone: false }]);
+      setTodo("");
+    }
+  };
+
+  console.log(todoList);
 
   return (
     <div className="App">
       <span className='heading'>Taskify</span>
-      <InputField todo={ todo } setTodo={ setTodo } />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={ handleAdd } />
 
     </div>
   );
