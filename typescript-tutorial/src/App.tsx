c089e560178213f7eb1bc62a1761e737a72a71f9
coligorm <colinrhysgorman@gmail.com>
2025-04-10 17:13:33 +1000

    - Created Todo model - Created TodoList state

--- typescript-tutorial/src/App.tsx
+++ typescript-tutorial/src/App.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import './App.css';
 import InputField from "./components/InputField"
+import { Todo } from './models/Todo';
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
+  const [todoList, setTodoList] = useState<Todo[]>([]);
 
   console.log(todo);
 
