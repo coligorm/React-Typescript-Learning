b139085f09986f4330defa8088710ef60b3f6577
coligorm <colinrhysgorman@gmail.com>
2025-04-10 17:01:09 +1000

    - Created basic UI - Created input box - Added CSS style

--- typescript-tutorial/src/App.tsx
+++ typescript-tutorial/src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import InputField from "./components/InputField"
 
-function App() {
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+
+  console.log(todo);
+
   return (
     <div className="App">
-      Hello World
+      <span className='heading'>Taskify</span>
+      <InputField todo={ todo } setTodo={ setTodo } />
+
     </div>
   );
 }
