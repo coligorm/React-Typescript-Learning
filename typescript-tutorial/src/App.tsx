1b05a2712e3cba5f4226077ba5932222200c6e3d
coligorm <colinrhysgorman@gmail.com>
2025-04-11 13:48:40 +1000

    - Created SingleToDo to hanlde individual todo state - Created ToDoList to hold a list of single todos - Updated style sheet to display SingleTodo and TodoList in a responsive way

--- typescript-tutorial/src/App.tsx
+++ typescript-tutorial/src/App.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import './App.css';
-import InputField from "./components/InputField"
+import InputField from './components/InputField';
 import { Todo } from './models/Todo';
+import TodoList from './components/TodoList';
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
@@ -19,13 +20,11 @@ const App: React.FC = () => {
   console.log(todos);
 
   return (
-    <div className="App">
+    <div className='App'>
       <span className='heading'>Taskify</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={ handleAdd } />
-      {/* <TodoList/> */}
-      {todos.map((t) => (
-        <li>{t.todo}</li>
-      ))}
+      <TodoList todos={todos} setTodos={setTodos}/>
+      
 
     </div>
   );
